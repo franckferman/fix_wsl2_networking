59c44aa36b1d744ecb7bade5ff15154d9d3608a1
Franck FERMAN <73023545+franckferman@users.noreply.github.com>
2022-03-13 16:42:30 +0100

    Add files via upload

---
+++ fix_wsl2_networking.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+Check_AdminRights()
+{
+UID_ROOT=0
+ERROR=77
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+if [ "$UID" -ne "$UID_ROOT" ]
+then
+  echo "${red}Sorry, you must run this script as root.${reset}"
+  exit $ERROR
+fi
+
+if [ -n "$1" ]
+then
+  echo "No parameters are supported by this script. Please run this without parameters."
+  exit $ERROR
+else
+  main
+fi
+}
+
+main()
+{
+SUCCESSFUL=0
+sudo chattr -i /etc/wsl.conf>/dev/null
+sudo rm /etc/wsl.conf>/dev/null
+
+sudo echo "[network]">/etc/wsl.conf
+sudo echo "generateResolvConf=false">>/etc/wsl.conf
+sudo chattr +i /etc/wsl.conf
+
+sudo chattr -i /etc/resolv.conf>/dev/null
+sudo rm /etc/resolv.conf
+
+sudo echo "nameserver 1.1.1.1">/etc/resolv.conf
+sudo echo "nameserver 1.0.0.1">>/etc/resolv.conf
+sudo chattr +i /etc/resolv.conf
+
+exit $SUCCESSFUL
+}
+
+Check_AdminRights
