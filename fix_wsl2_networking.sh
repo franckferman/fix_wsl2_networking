3c513c6bba8bc0dabb6b14b6649a508f3462de45
Franck FERMAN <73023545+franckferman@users.noreply.github.com>
2022-04-06 23:03:07 +0200

    Add files via upload

--- fix_wsl2_networking.sh
+++ fix_wsl2_networking.sh
@@ -12,7 +12,7 @@ reset=`tput sgr0`
 
     if [ "$UID" -ne "$uid_root" ]
         then
-        echo -n ${red}Error, please run the script with the command ${green}sudo ./fix_wsl2_networking.sh${reset}
+        echo -n "${red}Error, please run the script with the command ${green}sudo ./fix_wsl2_networking.sh${reset}"
         exit $error
     else
         main
@@ -23,14 +23,48 @@ main()
 {
 success=0
 
+clear
+echo "Which DNS servers do you want to choose?"
+echo 
+echo "1 - Cloudflare."
+echo "2 - Google."
+echo "3 - FDN (French Data Network)."
+echo "4 - Quad9."
+echo
+read -p "Your choice: " userChoice
+
+    if [ "$userChoice" -eq "1" ]
+        then
+        primDNS="1.1.1.1"
+        secDNS="1.0.0.1"
+    fi
+
+    if [ "$userChoice" -eq "2" ]
+        then
+        primDNS="8.8.8.8"
+        secDNS="8.8.4.4"
+    fi
+
+    if [ "$userChoice" -eq "3" ]
+        then
+        primDNS="80.67.169.12"
+        secDNS="80.67.169.40"
+    fi
+
+    if [ "$userChoice" -eq "4" ]
+        then
+        primDNS="9.9.9.9"
+        secDNS="149.112.112.112"
+    fi
+
 sudo echo [network]>/etc/wsl.conf
-sudo echo -n generateResolvConf=false>>/etc/wsl.conf
+sudo echo -n "generateResolvConf=false">>/etc/wsl.conf
 sudo chattr +i /etc/wsl.conf
 
 sudo rm /etc/resolv.conf
 sudo touch /etc/resolv.conf
-sudo echo "nameserver 1.1.1.1">/etc/resolv.conf
-sudo echo "nameserver 1.0.0.1">>/etc/resolv.conf
+sudo echo "nameserver $primDNS">/etc/resolv.conf
+sudo echo "nameserver $secDNS">>/etc/resolv.conf
 sudo chattr +i /etc/resolv.conf
 
 exit $success
