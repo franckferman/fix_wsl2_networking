431179bcff3bbee71440ed9c762520dc7e0756b8
Franck FERMAN <73023545+franckferman@users.noreply.github.com>
2023-03-19 15:28:35 +0100

    Add files via upload

--- fix_wsl2_networking.sh
+++ fix_wsl2_networking.sh
@@ -1,67 +1,106 @@
 #!/usr/bin/env bash
 # fix_wsl2_networking.sh
 
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-reset=$(tput sgr0)
+set -euo pipefail
 
-error=77
+readonly PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-check_admin_rights() {
-    uid_root=0
-
-    if [ "$UID" -ne "$uid_root" ]; then
-        echo "${red}Error, please run the script with sudo.${reset}"
-        exit $error
-    else
-        main
-    fi
+set_vars() {
+  prim_dns=""
+  sec_dns=""
+  readonly WSL_CONF_DIR="/etc/wsl.conf"
+  readonly RESOLV_CONF_DIR="/etc/resolv.conf"
 }
 
-main() {
-    wsl_conf_dir="/etc/wsl.conf"
-    resolv_conf_dir="/etc/resolv.conf"
-    success=0
+log_error() {
+  local message="$1"
+  readonly LOG_FILE="/var/log/fix_wsl2_networking.log"
+  local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
-    clear
+  echo "${timestamp} ERROR: ${message}" >> "$LOG_FILE"
+}
+
+select_dns_servers() {
+    declare -A dns_servers=(
+        ["Cloudflare"]="1.1.1.1, 1.0.0.1"
+        ["Google"]="8.8.8.8, 8.8.4.4"
+        ["FDN"]="80.67.169.12, 80.67.169.40"
+	["Digitalcourage"]="85.214.20.141, 46.182.19.48"
+        ["Quad9"]="9.9.9.9, 149.112.112.112"
+        ["OpenDNS"]="208.67.222.222, 208.67.220.220"
+	["DNSWatch"]="84.200.69.80, 84.200.70.40"
+	["Comodo"]="8.26.56.26, 8.20.247.20"
+	["Mullvad"]="100.64.0.31, 100.64.0.30"
+    )
+
     echo "Which DNS servers do you want to choose?"
     echo 
-    echo "1 - Cloudflare."
-    echo "2 - Google."
-    echo "3 - FDN (French Data Network)."
-    echo "4 - Quad9."
+    count=1
+    for server in "${!dns_servers[@]}"; do
+        echo "${count} - ${server} (${dns_servers[$server]})"
+        ((count++))
+    done
     echo
-    read -rp "Your choice: " user_choice
 
-    case "$user_choice" in
-        1) prim_dns="1.1.1.1"; sec_dns="1.0.0.1" ;;
-        2) prim_dns="8.8.8.8"; sec_dns="8.8.4.4" ;;
-        3) prim_dns="80.67.169.12"; sec_dns="80.67.169.40" ;;
-        4) prim_dns="9.9.9.9"; sec_dns="149.112.112.112" ;;
-        *)
-            echo "${red}Error, please select a valid choice: Only a number (from the list of choices) is expected as an answer.${reset}"
-            exit $error
-            ;;
-    esac
+	read -rp "Your choice: " user_choice
+	if ! [[ "$user_choice" =~ ^[1-9][0-9]*$ ]] || (( user_choice < 1 )) || (( user_choice > ${#dns_servers[@]} )); then
+    		log_error "Invalid input: $user_choice"
+    		echo "Error: Invalid input. Please enter a number between 1 and ${#dns_servers[@]}." >&2
+    		exit 1
+	fi
 
-    [ -f "$wsl_conf_dir" ] && sudo chattr -i "$wsl_conf_dir" &> "/dev/null" && sudo rm -f "$wsl_conf_dir"
+    local selected_dns=(${dns_servers[$(echo "${!dns_servers[@]}" | tr ' ' '\n' | sed -n "${user_choice}p")]//,/ })
+    PRIM_DNS="${selected_dns[0]}"
+    SEC_DNS="${selected_dns[1]}"
+}
 
-    sudo bash -c "cat > \"$wsl_conf_dir\" <<- EOM
+setup_wsl_conf() {
+  if [[ -f "$WSL_CONF_DIR" ]]; then
+  
+    if [[ "$(lsattr -d "$WSL_CONF_DIR" | cut -c 5)" == "i" ]]; then
+      chattr -i "$WSL_CONF_DIR" &> "/dev/null"
+    fi
+
+    rm -f "$WSL_CONF_DIR"
+  fi
+
+  bash -c "cat > \"$WSL_CONF_DIR\" <<- EOM
 [network]
 generateResolvConf=false
 EOM"
-    sudo chattr +i "$wsl_conf_dir" &> "/dev/null"
 
-    [ -f "$resolv_conf_dir" ] && sudo chattr -i "$resolv_conf_dir" &> "/dev/null" && sudo rm -f "$resolv_conf_dir"
-
-    sudo bash -c "cat > \"$resolv_conf_dir\" <<- EOM
-nameserver $prim_dns
-nameserver $sec_dns
-EOM"
-    sudo chattr +i "$resolv_conf_dir" &> "/dev/null"
-
-    exit $success
+  chattr +i "$WSL_CONF_DIR" &> "/dev/null"
 }
 
-check_admin_rights
+setup_resolv_conf() {
+  if [[ -f "$RESOLV_CONF_DIR" ]]; then
+
+    if [[ "$(lsattr -d "$RESOLV_CONF_DIR" | cut -c 5)" == "i" ]]; then
+      chattr -i "$RESOLV_CONF_DIR" &> "/dev/null"
+    fi
+
+    rm -f "$RESOLV_CONF_DIR"
+  fi
+
+  bash -c "cat > \"$RESOLV_CONF_DIR\" <<- EOM
+nameserver $PRIM_DNS
+nameserver $SEC_DNS
+EOM"
+  chattr +i "$RESOLV_CONF_DIR" &> "/dev/null"
+}
+
+main() {
+set_vars
+select_dns_servers
+setup_wsl_conf
+setup_resolv_conf
+exit 0
+}
+
+if [[ $EUID -ne 0 ]]; then
+	echo "Error: This script requires administrator privileges. Please run this script with sudo or as root." >&2
+	exit 1
+fi
+
+main
 
